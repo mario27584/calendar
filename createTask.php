6c5abf825954d9c6e2c8d7ae779244f1a67249c1
mario27584 <mario27584@gmail.com>
2015-11-05 00:01:37 -0700

    ?

--- createTask.php
+++ createTask.php
@@ -9,6 +9,7 @@ if(isset($_POST['submit']))
 
 //      open connection to mysql db
 $conn = mysqli_connect($host,$user,$pass,$db) or die("@@@@Error " . mysqli_error($conn));
+//$conn = mysqli_connect("localhost","root","","schedule") or die("Error " . mysqli_error($conn));
 
 $name = $_POST['name'];
 //echo "name: ".$name;
