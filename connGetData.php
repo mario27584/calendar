7c6b8196654e0a5fc26a2c740bd5e1812774724f
mario27584 <mario27584@gmail.com>
2015-11-07 14:15:15 -0700

    fix adding task by deleting all the values in db

--- connGetData.php
+++ connGetData.php
@@ -5,9 +5,9 @@ include 'credentials.php';
 
 //      open connection to mysql db
 
-//$connection = mysqli_connect($host,$user,$pass,$db) or die("Error " . mysqli_error($connection));
+$connection = mysqli_connect($host,$user,$pass,$db) or die("Error " . mysqli_error($connection));
 
-$connection = mysqli_connect("localhost","root","","schedule") or die("Error " . mysqli_error($connection));
+//$connection = mysqli_connect("localhost","root","","schedule") or die("Error " . mysqli_error($connection));
 
 
       //fetch table rows from mysql db
@@ -25,7 +25,7 @@ while($row = mysqli_fetch_assoc($result))
          
       }
    
-    echo json_encode($class_array);
+  //  echo json_encode($class_array);
       
      echo "<script> var tasks=".json_encode($class_array)."</script>";
  
